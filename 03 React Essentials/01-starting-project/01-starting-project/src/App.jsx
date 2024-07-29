952aca4f8bd0ab0db79a8087418f29eebaa6bc41
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-07-29 23:16:38 +0530

    special Children

--- 03 React Essentials/01-starting-project/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/01-starting-project/src/App.jsx
@@ -2,6 +2,7 @@ import componentImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
@@ -16,6 +17,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>PROPS</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
--- 03 React Essentials/01-starting-project/01-starting-project/src/components/CoreConcept.jsx
+++ 03 React Essentials/01-starting-project/01-starting-project/src/components/CoreConcept.jsx
@@ -1,3 +1,5 @@
+import "./coreConcepts.css";
+
 export default function CoreConcept({ image, title, description }) {
   return (
     <li>
