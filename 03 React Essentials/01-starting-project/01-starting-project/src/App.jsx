041118d2ba9fd750bb730bce272b72b20640c99a
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-07-29 22:40:23 +0530

    Header

--- 03 React Essentials/01-starting-project/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/01-starting-project/src/App.jsx
@@ -8,20 +8,6 @@ function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
 function CoreConcept({ image, title, description }) {
   return (
     <li>
@@ -31,10 +17,6 @@ function CoreConcept({ image, title, description }) {
     </li>
   );
 }
-gggrrtrtrg;
-//fgeggrtrttrtth
-gtrrggr;
-trggtr;
 
 function App() {
   return (
