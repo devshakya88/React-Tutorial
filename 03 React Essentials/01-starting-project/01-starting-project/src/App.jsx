5f8f4e6b2ce5117854200aa8f42a7d3e8b61b5af
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-07-30 09:47:58 +0530

    use State

--- 03 React Essentials/01-starting-project/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/01-starting-project/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import componentImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header.jsx";
@@ -5,11 +6,12 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  let tabContent = "Please Click a button";
+  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
   function handleSelect(selectedButton) {
-    tabContent = selectedButton;
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
   }
+  console.log("App component");
   return (
     <div>
       <Header />
@@ -33,7 +35,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {tabContent}
+          {selectedTopic}
         </section>
       </main>
     </div>
