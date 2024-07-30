1cf217553a569ee339b364c1bb7e272935a53152
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-07-30 09:23:13 +0530

    test

--- 03 React Essentials/01-starting-project/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/01-starting-project/src/App.jsx
@@ -5,8 +5,10 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  function handleSelect() {
-    console.log("Hello World");
+  let tabContent = "Please Click a button";
+  function handleSelect(selectedButton) {
+    tabContent = selectedButton;
+    console.log(selectedButton);
   }
   return (
     <div>
@@ -24,12 +26,14 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          Dynamic
+          {tabContent}
         </section>
       </main>
     </div>
